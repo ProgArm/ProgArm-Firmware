7b738e7b9277bc274f9b1df2845c5eb7b374634e
Alex Jakimenko <alex.jakimenko@gmail.com>
2015-02-13 23:53:58 +0200

    RTC alarm

--- src/core/rtc.cpp
+++ src/core/rtc.cpp
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+#include <misc.h>
 #include <stm32f10x.h>
 #include <stm32f10x_bkp.h>
 #include <stm32f10x_pwr.h>
@@ -20,10 +21,10 @@
 #include <stm32f10x_rtc.h>
 #include <tuple>
 
+
 void RTC_Configuration(void) {
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
     PWR_BackupAccessCmd(ENABLE); // Allow access to BKP Domain
-
     BKP_DeInit(); // Reset Backup Domain // TODO move to backup.cpp?
 
     RCC_LSEConfig(RCC_LSE_ON); // Enable LSE
@@ -34,9 +35,6 @@ void RTC_Configuration(void) {
     RCC_RTCCLKCmd(ENABLE);
     RTC_WaitForSynchro();
     RTC_WaitForLastTask();
-
-    //RTC_ITConfig(RTC_IT_SEC, ENABLE);
-    //RTC_WaitForLastTask();
 
     // Set RTC prescaler: set RTC period to 1sec
     RTC_SetPrescaler(32767); // RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1)
@@ -54,9 +52,31 @@ void RTC_Setup() {
             //External Reset occurred...
         }
         RTC_WaitForSynchro();
-        //RTC_ITConfig(RTC_IT_SEC, ENABLE);
-        // RTC_WaitForLastTask(); // Wait until last write operation on RTC registers has finished
+        RTC_ITConfig(RTC_IT_ALR, ENABLE);
+        RTC_WaitForLastTask(); // Wait until last write operation on RTC registers has finished
         RCC_ClearFlag();
+    }
+
+    //NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    // Enable the RTC Interrupt
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = RTC_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    //RTC_ClearITPendingBit(RTC_IT_ALR);
+    RCC_ClearFlag();
+}
+
+
+extern "C" void RTC_IRQHandler(void) {
+    if (RTC_GetITStatus(RTC_IT_ALR) != RESET) {
+        RTC_WaitForLastTask();
+        RTC_ClearITPendingBit(RTC_IT_ALR);
+        RTC_WaitForLastTask();
+        // TODO
     }
 }
 
